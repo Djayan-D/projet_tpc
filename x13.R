411f4f7be7d411dc26d93e080204efbac5ac71d4
Arthur Ernoul De La Provôté <arthur.edlp@outlook.fr>
2025-04-08 15:47:34 +0200

    mise en place d'une deuxième méthode

--- x13.R
+++ x13.R
@@ -19,4 +19,22 @@ forecast_x13 <- forecast(arima_model, h = 12)
 # Visualisation
 autoplot(forecast_x13)
 
+## --------- Autre méthode -------## 
+
+# on continuera l'étude avec cette méthode là
+# car permet prévision
+
+library(RJDemetra)
+
+myspec <- x13_spec("RSA5c")
+
+mysax13 <- x13(ts_freq_mens_cinema_0020_corr, myspec)
+
+summary(mysax13$regarima)
+
+mysax13
+
+plot(mysax13$final)
+
+
 
