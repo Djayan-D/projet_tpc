ef3d4feb79edf7bc351852e5593a4ba51b79b6ef
Arthur Ernoul De La Provôté <arthur.edlp@outlook.fr>
2025-04-22 14:18:11 +0200

    commentaire STS mis à jour

--- Modèles/STS.R
+++ Modèles/STS.R
@@ -16,13 +16,13 @@ plot(prevsts) # en annexe
 ## 1.093e+12  2.087e+08  8.152e+11  1.803e+12
 ## 
 ## level = 1.093e+12 : Cette variance élevée signifie que les fluctuations autour du niveau moyen sont très importantes.
-## slope = 2.087e+08 : Une variance plus faible suggère que la tendance est relativement stable.
+## slope = 2.087e+08 : Une variance plus faible, que celle précédente, suggère que la tendance est relativement stable.
 ## seas = 8.152e+11 : Une variance significative indique une forte composante saisonnière dans les données.
 ## epsilon = 1.803e+12 : Une variance élevée indique qu'il reste des variations non expliquées par le modèle.
 ##
 ## Conclusion:
 ## Les variances indiquent que la série est principalement influencée par des fluctuations
-## autour du niveau moyen et des variations saisonnières. La pente
+## autour du niveau moyen et des variations saisonnières ainsi que celle des résidus. La pente
 ## contribue moins à la variabilité globale. La variance élevée des résidus suggère
 ## qu'il reste des variations non expliquées par le modèle
 
